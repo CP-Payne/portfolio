1d6c2908a8e5b9aff7dcb74171d780bd6031f358
Charles <charlethical1@gmail.com>
2025-05-23 13:53:26 +0200

    feat: Introduce main wrapper for sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import Navbar from "./Components/Navbar/Navbar"
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Sections/Home/Home";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <main className="w-full pt-[10v]">
+        <Home />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
