d3a020839f26ab97f5dba0ebf2134c732e564de3
Charles <charlethical1@gmail.com>
2025-05-24 15:55:03 +0200

    feat: implement skill section

--- src/Components/SkillCard/SkillCard.tsx
+++ src/Components/SkillCard/SkillCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SkillCard = ({ name, image }: Props) => {
   return (
-    <div className="shadow-md shadow-[#040c16] bg-mainLightGray hover:scale-110 duration-500 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg  w-56 h-44 p-5 flex flex-col items-center">
+    <div className="shadow-md shadow-[#040c16] bg-mainLightGray hover:scale-110 duration-500 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg h-32  md:w-56 md:h-44 p-5 flex flex-col items-center">
       <div className="h-[60%] w-auto">
         <img
           src={image}
