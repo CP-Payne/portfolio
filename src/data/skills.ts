4f14cc299ed967d69faeb1fc8359c6a3d7c3c327
Charles <charlethical1@gmail.com>
2025-05-24 15:15:12 +0200

    feat: add skill and skill data mapping

---
+++ src/data/skills.ts
@@ -0,0 +1,14 @@
+const skillList = [
+    {name: "Go", image: "go.png"},
+    {name: "Docker", image: "docker.png"},
+    {name: "JavaScript", image: "javascript.png"},
+    {name: "Postgres", image: "postgress.png"},
+    {name: "Python", image: "python.png"},
+    {name: "React", image: "react.png"},
+    {name: "Tailwind", image: "tailwind.png"},
+    {name: "TypeScript", image: "typescript.png"},
+    {name: "C#", image: "csharp.png"},
+    {name: "ASP.NET Core", image: "dotnetcore.png"},
+]
+
+export default skillList;
