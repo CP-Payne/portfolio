606facbbfa8bcb40f93f90e1d588917cc3e22762
Charles <charlethical1@gmail.com>
2025-05-24 15:35:38 +0200

    feat: implement skill card component

---
+++ src/Components/SkillCard/SkillCard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface Props {
+  name: string;
+  image: string;
+}
+
+const SkillCard = ({ name, image }: Props) => {
+  return (
+    <div className="shadow-md shadow-[#040c16] bg-mainLightGray hover:scale-110 duration-500 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg  w-56 h-44 p-5 flex flex-col items-center">
+      <div className="h-[60%] w-auto">
+        <img
+          src={image}
+          alt={name + " icon"}
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <p className="mt-6">{name}</p>
+    </div>
+  );
+};
+
+export default SkillCard;
