4655a02911ab0f7c18b8c286d288b64a9261c727
Charles <charlethical1@gmail.com>
2025-05-24 05:42:41 +0200

    style: implement Raleway font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif']
+      },
       colors: {
         mainDarkGray: "#111111",
         mainLightGray: "#212121",
