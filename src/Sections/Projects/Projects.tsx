b96d3982cbc51c982b9f92ea7f9d4cfc94ab0344
Charles <charlethical1@gmail.com>
2025-05-25 13:17:29 +0200

    feat: implement project section

--- src/Sections/Projects/Projects.tsx
+++ src/Sections/Projects/Projects.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import ProjectCard from "../../Components/ProjectCard/ProjectCard";
+import projectList from "../../data/projects";
 
 type Props = {};
 
+const getImage = (fileName: string) => {
+  return new URL(`../../assets/Projects/${fileName}`, import.meta.url).href;
+};
+
 const Projects = (props: Props) => {
-  return <div className="bg-purple-400">Projects</div>;
+  return (
+    <section className="bg-mainLightGray min-h-[90vh] text-gray-300 w-full flex items-center pt-28 lg:pt-0">
+      <div className=" max-w-5xl mx-auto flex flex-col">
+        <h1 className="border-b-4 border-mainGoldColor w-fit text-2xl md:text-4xl font-bold">
+          Projects
+        </h1>
+        <div className=" w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 py-8 px-2 gap-7">
+          {projectList.map((project) => (
+            <ProjectCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              image={getImage(project.image)}
+              stack={project.stack}
+              link={project.link}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
