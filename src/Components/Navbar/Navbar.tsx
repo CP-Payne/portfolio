5b2b16895df6b6ce983da096f2c18a1223c6456d
Charles <charlethical1@gmail.com>
2025-05-26 12:11:07 +0200

    feat: add mobile navbar menu icon

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./initials_logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <div className="fixed bg-mainLightGray w-full flex justify-between h-[10vh] items-center text-gray-300 ">
       <img
@@ -11,7 +14,7 @@ const Navbar = (props: Props) => {
         alt="Initials Logo"
         className="ml-5 w-[5rem] mt-6 lg:mt-0"
       />
-      <ul className="flex w-[30%] justify-center gap-11 font-bold text-[1.2rem] mt-6 lg:mt-0">
+      <ul className=" hidden md:flex w-[30%] justify-center gap-11 font-bold text-[1.2rem] mt-6 lg:mt-0">
         <li>
           <a href="#home">Home</a>
         </li>
@@ -28,6 +31,11 @@ const Navbar = (props: Props) => {
           <a href="#">Contact</a>
         </li>
       </ul>
+      {/* Hamburger Menu Icon */}
+      <div className="md:hidden mr-6" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
     </div>
   );
 };
