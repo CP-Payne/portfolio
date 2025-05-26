d12956c8af9996d0f0a43d721e770a175633ebcd
Charles <charlethical1@gmail.com>
2025-05-26 12:20:19 +0200

    style: navbar links more responsive

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = (props: Props) => {
         alt="Initials Logo"
         className="ml-5 w-[5rem] mt-6 lg:mt-0"
       />
-      <ul className=" hidden md:flex w-[30%] justify-center gap-11 font-bold text-[1.2rem] mt-6 lg:mt-0">
+      <ul className=" hidden md:flex flex-wrap justify-center gap-9 font-bold text-[1.2rem] mt-6 lg:mt-0 mr-12">
         <li>
           <a href="#home">Home</a>
         </li>
