32cd3c60910fa4068220b1e884596ac43dccbd5c
Charles <charlethical1@gmail.com>
2025-05-26 12:05:51 +0200

    style: adjusted navbar link space

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = (props: Props) => {
         alt="Initials Logo"
         className="ml-5 w-[5rem] mt-6 lg:mt-0"
       />
-      <ul className="flex w-[40%] justify-evenly font-bold text-[1.2rem] mt-6 lg:mt-0">
+      <ul className="flex w-[30%] justify-center gap-11 font-bold text-[1.2rem] mt-6 lg:mt-0">
         <li>
           <a href="#home">Home</a>
         </li>
